cd740c17099417212d417fc16bd285e27b30d758
Bruna <brunaalmeida075@gmail.com>
2023-03-29 10:42:55 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Portifolio de Bruna na area
+          Portifolio d
         </p>
         <a
           className="App-link"
