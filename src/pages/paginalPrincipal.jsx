7e094b4f20a410d438ba18fd04129ed5dd752425
Bruna <brunaalmeida075@gmail.com>
2023-03-29 10:59:53 -0300

    adicionando pagina

---
+++ src/pages/paginalPrincipal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import BarraNav from '../componentes/barraNav';
+import Conhecimento from '../componentes/conteinerConhecimento';
+import Projetos from '../componentes/conteinerProjetos';
+import SobreMim from '../componentes/conteinerSobreMim';
+import Footer from "../componentes/footer";
+
+export default function Principal(){
+    return(
+        <div>
+            <BarraNav />
+            <SobreMim />
+            <Conhecimento />
+            <Projetos />
+            <Footer />
+        </div>
+    )
+}
